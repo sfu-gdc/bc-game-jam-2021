f40fcf8899eb61cf72fcdf67d80441031cbfc27a
Spencer Leung <spencerleung1024@gmail.com>
2021-09-18 16:45:55 -0700

    Fixed broken stuff from pulling.

--- BCGameJam/World/Camera2D.gd
+++ BCGameJam/World/Camera2D.gd
@@ -9,10 +9,6 @@ func _ready():
 	background.position = Vector2(-0.5 * background.scale.x, -0.5 * background.scale.y)
 
 func _process(delta):
-<<<<<<< Updated upstream
 	zoom += 0.02 * zoom.x * Vector2(delta, delta)
-=======
-	zoom += 0.05 * zoom.x * Vector2(delta, delta)
 	background.scale = Vector2(screen_size.x * zoom.x, screen_size.y * zoom.y)
 	background.position = Vector2(-0.5 * background.position.x, -0.5 * background.position.y)
->>>>>>> Stashed changes
